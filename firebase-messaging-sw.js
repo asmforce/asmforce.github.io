c91e55affbb4dd54268be37507767591f9d35b34
Vitaliy Krutko <vitaliy.krutko@rozdoum.com>
2019-07-23 16:45:47 +0300

    Another attempts to make this thing work

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,1 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-
-firebase.initializeApp({messagingSenderId: 'blockwrk-9da2b'});
-
-firebase.messaging().setBackgroundMessageHandler((payload) => {
-  console.log('Message received. ', payload);
-});
+console.log('firebase-messaging-sw.js executed');
--- messaging.js
+++ messaging.js
@@ -1,6 +1,18 @@
-firebase.initializeApp({messagingSenderId: 'blockwrk-9da2b'});
+firebase.initializeApp({
+  apiKey: "AIzaSyDd7CliZSWJZcPhZxv-TuI4R32AOVEKK6c",
+  authDomain: "blockwrk-9da2b.firebaseapp.com",
+  databaseURL: "https://blockwrk-9da2b.firebaseio.com",
+  projectId: "blockwrk-9da2b",
+  storageBucket: "",
+  messagingSenderId: "914157587718",
+  appId: "1:914157587718:web:d2e56141da1ff302"
+});
 
 const messaging = firebase.messaging();
+
+messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);
+});
 
 messaging.getToken().then((newToken) => {
   console.log('New token: ', newToken);
