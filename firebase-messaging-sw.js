621881ce1103e7d55f1e767e4639dcc4cf35ec78
Vitaliy Krutko <vitaliy.krutko@rozdoum.com>
2019-07-23 18:18:59 +0300

    Another attempts to make this thing work in background

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,18 @@
-console.log('firebase-messaging-sw.js executed');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDd7CliZSWJZcPhZxv-TuI4R32AOVEKK6c",
+  authDomain: "blockwrk-9da2b.firebaseapp.com",
+  databaseURL: "https://blockwrk-9da2b.firebaseio.com",
+  projectId: "blockwrk-9da2b",
+  storageBucket: "",
+  messagingSenderId: "914157587718",
+  appId: "1:914157587718:web:d2e56141da1ff302"
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler((payload) => {
+  console.log('Background message received. ', payload);
+});
