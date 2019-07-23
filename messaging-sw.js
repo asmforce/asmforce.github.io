33aa2696e51a5b02eb8ac2cbd5a7dc2dca4e1707
Vitaliy Krutko <vitaliy.krutko@rozdoum.com>
2019-07-23 16:21:22 +0300

    Another attempt to make this thing work

--- messaging-sw.js
+++ messaging-sw.js
@@ -3,18 +3,6 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 firebase.initializeApp({messagingSenderId: 'blockwrk-9da2b'});
 
-const messaging = firebase.messaging();
-
-messaging.setBackgroundMessageHandler((payload) => {
+firebase.messaging().setBackgroundMessageHandler((payload) => {
   console.log('Message received. ', payload);
-});
-
-messaging.getToken().then((newToken) => {
-  console.log('New token: ', newToken);
-});
-
-messaging.onTokenRefresh(() => {
-  messaging.getToken().then((newToken) => {
-    console.log('New token: ', newToken);
-  });
 });
--- messaging.js
+++ messaging.js
@@ -2,6 +2,20 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/messaging-sw.js').then(function(registration) {
       console.log('Service worker registered with scope: ' + registration.scope);
+
+      firebase.initializeApp({messagingSenderId: 'blockwrk-9da2b'});
+
+      const messaging = firebase.messaging();
+
+      messaging.getToken().then((newToken) => {
+        console.log('New token: ', newToken);
+      });
+
+      messaging.onTokenRefresh(() => {
+        messaging.getToken().then((newToken) => {
+          console.log('New token: ', newToken);
+        });
+      });
     }, function(error) {
       console.log('Service worker registration failed: ', error);
     });
