1dd2608641d1ff0cb47c046e95060c824a1e64c7
Vitaliy Krutko <vitaliy.krutko@rozdoum.com>
2019-07-23 16:16:54 +0300

    Fixed undefined variable

--- messaging-sw.js
+++ messaging-sw.js
@@ -3,7 +3,9 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 firebase.initializeApp({messagingSenderId: 'blockwrk-9da2b'});
 
-firebase.messaging().setBackgroundMessageHandler((payload) => {
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler((payload) => {
   console.log('Message received. ', payload);
 });
 
