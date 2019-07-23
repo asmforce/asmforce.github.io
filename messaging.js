5583b7b71bc163408a0ec5226aec267b977594b7
Vitaliy Krutko <vitaliy.krutko@rozdoum.com>
2019-07-23 16:27:13 +0300

    Another attempt to make this thing work

--- messaging.js
+++ messaging.js
@@ -1,23 +1,13 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/messaging-sw.js').then(function(registration) {
-      console.log('Service worker registered with scope: ' + registration.scope);
+firebase.initializeApp({messagingSenderId: 'blockwrk-9da2b'});
 
-      firebase.initializeApp({messagingSenderId: 'blockwrk-9da2b'});
+const messaging = firebase.messaging();
 
-      const messaging = firebase.messaging();
+messaging.getToken().then((newToken) => {
+  console.log('New token: ', newToken);
+});
 
-      messaging.getToken().then((newToken) => {
-        console.log('New token: ', newToken);
-      });
-
-      messaging.onTokenRefresh(() => {
-        messaging.getToken().then((newToken) => {
-          console.log('New token: ', newToken);
-        });
-      });
-    }, function(error) {
-      console.log('Service worker registration failed: ', error);
-    });
+messaging.onTokenRefresh(() => {
+  messaging.getToken().then((newToken) => {
+    console.log('New token: ', newToken);
   });
-}
+});
